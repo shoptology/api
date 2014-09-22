3292d8672f706fe1972742198f022a22360bda8e
ivanmayes <ivanmayes@gmail.com>
2014-09-22 16:01:31 -0500

    Switched to swagger-node-express

--- models/sample.js
+++ models/sample.js
@@ -1,31 +1,4 @@
-// Load module dependencies.
-var express = require("express"),
- url = require("url"),
- bodyParser = require('body-parser'),
- session = require('express-session'),
- MongoStore = require('connect-mongo')({session:session}),
- mongoose = require('mongoose'),
- json = require('express-json'),
- swagger = require("swagger-node-express");
-
-// Create the application.
-var app = express()
-	.use(json());
-app.use(bodyParser.urlencoded());
-
-/**
- * Connect to MongoDB.
- */
-
-mongoose.connect('mongodb://localhost:27017/test');
-mongoose.connection.on('error', function() {
-    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
-});
-
-// Couple the application to the Swagger module.
-swagger.setAppHandler(app);
-
-swagger.addModels({
+exports.models = {
     "Category":{
       "id":"Category",
       "required": ["id", "name"],
@@ -102,38 +75,4 @@ swagger.addModels({
         }
       }
     }
-});
-
-var findById = {
-  'spec': {
-    "description" : "Operations about pets",
-    "path" : "/pet.{format}/{petId}",
-    "notes" : "Returns a pet based on ID",
-    "summary" : "Find pet by ID",
-    "method": "GET",
-    "parameters" : [swagger.pathParam("petId", "ID of pet that needs to be fetched", "string")],
-    "type" : "Pet",
-    "errorResponses" : [swagger.errors.invalid('id'), swagger.errors.notFound('pet')],
-    "nickname" : "getPetById"
-  },
-  'action': function (req,res) {
-    if (!req.params.petId) {
-      throw swagger.errors.invalid('id');
-    }
-    var id = parseInt(req.params.petId);
-    var pet = petData.getPetById(id);
-
-    if (pet) {
-      res.send(JSON.stringify(pet));
-    } else {
-      throw swagger.errors.notFound('pet');
-    }
-  }
-};
-
-swagger.addGet(findById);
-
-swagger.configure("http://0.0.0.0", "0.1");
-
-app.listen(8002);
-console.log('started');
+}
